65fdc084d7bf260e72a3bec479f721c73fb0f13a
thenetbeangang <Caorey13@hotmail.com>
2024-06-07 10:34:22 +0200

    Test

--- app/main.php
+++ app/main.php
@@ -1,5 +1,5 @@
 <?php
-// Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
+Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 
 // Disable output buffering.
 while (ob_get_level() !== 0) {
@@ -10,16 +10,16 @@ while (ob_get_level() !== 0) {
 echo "Logs from your program will appear here!\n";
 
 // Uncomment this to pass the first stage.
-// $child_pid = pcntl_fork();
-// if ($child_pid == -1) {
-//   echo "Error forking!";
-// }
-// elseif ($child_pid) {
-//   // We're in parent.
-//   pcntl_wait($status);
-//   echo "Child terminates!";
-// }
-// else {
-//   // Replace current program with calling program.
-//   echo exec(implode(' ', array_slice($argv, 3)));
-// }
+ $child_pid = pcntl_fork();
+ if ($child_pid == -1) {
+   echo "Error forking!";
+ }
+ elseif ($child_pid) {
+   // We're in parent.
+   pcntl_wait($status);
+   echo "Child terminates!";
+ }
+ else {
+   // Replace current program with calling program.
+   echo exec(implode(' ', array_slice($argv, 3)));
+ }
